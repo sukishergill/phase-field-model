117b67fed5a78b06e52f601c52719be95835484f
Suki Shergill <sks78@sfu.ca>
2024-08-27 15:29:36 -0700

    Update CH_SSAV_ex1.m

--- CH_SSAV_ex1.m
+++ CH_SSAV_ex1.m
@@ -1,3 +1,5 @@
+clear all;
+
 Para.alpha = 0;
 Para.beta = 1;
 Para.epsilon = 0.1;
@@ -18,20 +20,21 @@ y = Grid.Ly*(1:Grid.Ny)' / Grid.Ny - Grid.Ly/2;
 
 [xx,yy] = meshgrid(x,y);
 
-kx = [ 0:nx/2-1, 0.0, -nx/2+1:-1]' / (Lx/pi/2);
-ky = [ 0:ny/2-1, 0.0, -ny/2+1:-1]' / (Ly/pi/2);
+kx = [ 0:Grid.Nx/2-1, 0.0, -Grid.Nx/2+1:-1]' / (Grid.Lx/pi/2);
+ky = [ 0:Grid.Ny/2-1, 0.0, -Grid.Ny/2+1:-1]' / (Grid.Ly/pi/2);
 
 [kkx,kky] = meshgrid(kx, ky);
-Grid.k = sqrt(kkx.^2 + kky.^2);
+k = sqrt(kkx.^2 + kky.^2);
 Grid.k = k(:);
 
-Grid.inv_k = 1./(k.^2);      Grid.inv_k(k == 0) = 1;
+Grid.inv_k = 1./(Grid.k.^2);      Grid.inv_k(k == 0) = 1;
 
 
-dt = 0.001;
+dt_min = 0.01;
+dt_max = 0.01;
 tf = 8;
 
-t_vals = linspace(dt, tf, round(tf / dt));
+% t_vals = linspace(dt, tf, round(tf / dt));
 
 
 
@@ -41,7 +44,7 @@ u = 0.05*sin(xx).*sin(yy);
 
 
 [tt, uu, Eu, Eu_SSAV, Em, mass, m_est_vals, t_vals, dt_vals] = ...
-    SSAV_2D(Grid, dt, tf, Para, u, 1);
+    SSAV_2D(Grid,dt_min, dt_max, tf, Para, u, 1);
 
 
 figure(1)
