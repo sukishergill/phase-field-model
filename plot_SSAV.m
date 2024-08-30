77f5535f9ec869b4dae37d45363172565a63d863
Suki Shergill <sks78@sfu.ca>
2024-08-30 15:48:24 -0700

    Create plot_SSAV.m

---
+++ plot_SSAV.m
@@ -0,0 +1,43 @@
+% plotting
+
+figure(1)
+plot(tt, Eu - Em, 'Linewidth', 4)
+set(gca, 'Fontsize', 40)
+xlabel('Time', 'Interpreter','latex')
+title('$E(u,w)-E(m)$', 'Interpreter','latex')
+set(gca,'TickLabelInterpreter','latex')
+
+figure(2)
+
+subplot(2,2,1)
+surf(x,y,uu{26}), shading interp, lighting phong, axis equal,axis off;
+view([-90 90]);
+title(['t = ', num2str(tt(26))], 'Interpreter', 'latex')
+caxis([-1 , 1])
+set(gca, 'Fontsize', 40)
+set(gca,'TickLabelInterpreter','latex')
+subplot(2,2,2)
+surf(x,y,uu{51}), shading interp, lighting phong, axis equal,axis off;
+view([-90 90]);
+title(['t = ', num2str(tt(51))], 'Interpreter', 'latex')
+caxis([-1 , 1])
+set(gca, 'Fontsize', 40)
+set(gca,'TickLabelInterpreter','latex')
+subplot(2,2,3)
+surf(x,y,uu{76}), shading interp, lighting phong, axis equal,axis off;
+view([-90 90]);
+title(['t = ', num2str(tt(76))], 'Interpreter', 'latex')
+caxis([-1 , 1])
+set(gca, 'Fontsize', 40)
+set(gca,'TickLabelInterpreter','latex')
+subplot(2,2,4)
+surf(x,y,uu{101}), shading interp, lighting phong, axis equal,axis off;
+view([-90 90]);
+title(['t = ', num2str(tt(101))], 'Interpreter', 'latex')
+c = colorbar;
+set(c, 'TickLabelInterpreter','latex')
+set(c, 'Position', [0.9 0.168 0.022 0.7]);
+caxis([-1 , 1])
+set(gca, 'Fontsize', 40)
+set(gca,'TickLabelInterpreter','latex')
+colormap('gray')
