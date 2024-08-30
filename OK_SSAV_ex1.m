23e6f1eb1694a2b9514ba2a8c083b164f07f82ee
Suki Shergill <sks78@sfu.ca>
2024-08-30 15:48:38 -0700

    Update OK_SSAV_ex1.m

--- OK_SSAV_ex1.m
+++ OK_SSAV_ex1.m
@@ -50,48 +50,4 @@ u = Para.m + 0.001*rand(Grid.Nx, Grid.Ny);
 [tt, uu, Eu, Eu_SSAV, Em, mass, m_est_vals, t_vals, dt_vals] = ...
     SSAV_2D(Grid, Time, Para, u, model);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%% Plotting %%%%%%%%%%%%%%%%%%%% 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-figure(1)
-plot(tt, Eu - Em, 'Linewidth', 4)
-set(gca, 'Fontsize', 40)
-xlabel('Time', 'Interpreter','latex')
-title('$E(u,w)-E(m)$', 'Interpreter','latex')
-set(gca,'TickLabelInterpreter','latex')
-
-figure(2)
-
-subplot(2,2,1)
-surf(x,y,uu{26}), shading interp, lighting phong, axis equal,axis off;
-view([-90 90]);
-title(['t = ', num2str(tt(26))], 'Interpreter', 'latex')
-caxis([-1 , 1])
-set(gca, 'Fontsize', 40)
-set(gca,'TickLabelInterpreter','latex')
-subplot(2,2,2)
-surf(x,y,uu{51}), shading interp, lighting phong, axis equal,axis off;
-view([-90 90]);
-title(['t = ', num2str(tt(51))], 'Interpreter', 'latex')
-caxis([-1 , 1])
-set(gca, 'Fontsize', 40)
-set(gca,'TickLabelInterpreter','latex')
-subplot(2,2,3)
-surf(x,y,uu{76}), shading interp, lighting phong, axis equal,axis off;
-view([-90 90]);
-title(['t = ', num2str(tt(76))], 'Interpreter', 'latex')
-caxis([-1 , 1])
-set(gca, 'Fontsize', 40)
-set(gca,'TickLabelInterpreter','latex')
-subplot(2,2,4)
-surf(x,y,uu{101}), shading interp, lighting phong, axis equal,axis off;
-view([-90 90]);
-title(['t = ', num2str(tt(101))], 'Interpreter', 'latex')
-c = colorbar;
-set(c, 'TickLabelInterpreter','latex')
-set(c, 'Position', [0.9 0.168 0.022 0.7]);
-caxis([-1 , 1])
-set(gca, 'Fontsize', 40)
-set(gca,'TickLabelInterpreter','latex')
-colormap('gray')
+plot_SSAV
