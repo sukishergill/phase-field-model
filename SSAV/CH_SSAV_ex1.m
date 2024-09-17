9682a229c2bc27d92abfc5e64740b138ef19496d
Suki Shergill <sks78@sfu.ca>
2024-09-17 11:30:51 -0700

    Update CH_SSAV_ex1.m

--- SSAV/CH_SSAV_ex1.m
+++ SSAV/CH_SSAV_ex1.m
@@ -37,14 +37,15 @@ Grid.inv_k = 1./(Grid.k.^2);      Grid.inv_k(k == 0) = 1;
 % time discretization
 % if dt_min = dt_max BDF2 will be implemented, otherwise an adaptive time
 % stepping scheme will be used
-Time.dt_min = 0.01;        % minimum time step
+Time.dt_min = 0.0001;        % minimum time step
 Time.dt_max = 0.01;        % maximum time step
-Time.tf = 8;
+Time.tf = 2;
 
 % Initial condition
 u = 0.05*sin(xx).*sin(yy);
 
-[tt, uu, Eu, Eu_SSAV, Em, mass, m_est_vals, t_vals, dt_vals] = ...
+[u, Eu, Em, mass, m_est_vals, t_vals, dt_vals] = ...
     SSAV_2D(Grid, Time, Para, u, model);
 
-plot_SSAV
+% plot_SSAV
+% plot_err
