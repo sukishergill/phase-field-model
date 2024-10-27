e7b90bf69751c6496dd5e414ca9bbcb009d9c65a
Suki Shergill <sks78@sfu.ca>
2024-10-26 19:16:22 -0700

    Update FCH_BDF2_SAV.m

--- FCH/FCH_BDF2_SAV.m
+++ FCH/FCH_BDF2_SAV.m
@@ -8,16 +8,15 @@ function [ xx, yy, k, tt, uu, Eu, Em, mass] = FCH_BDF2_SAV ( nx, ny,...
 
 % spatial grid
 dx = Lx/nx;                         dy = Ly/ny;
-x = Lx*(1:nx)' / nx - Lx/2;         y = Ly*(1:ny)' / ny - Ly/2;      
-
+x = Lx*(1:nx)' / nx - Lx/2;         y = Ly*(1:ny)' / ny - Ly/2;
 [xx, yy] = meshgrid(x, y);
 
 kx = [ 0:nx/2-1, 0.0, -nx/2+1:-1]' / (Lx/pi/2);
 ky = [ 0:ny/2-1, 0.0, -ny/2+1:-1]' / (Ly/pi/2);
 
-[kkx,kky] = meshgrid(kx, ky);
-k = sqrt(kkx.^2 + kky.^2);
-k = k(:);
+[kxx,kyy] = meshgrid(kx, ky);
+k = sqrt(kxx.^2 + kyy.^2);
+% k = k(:);
 
 % define number of time steps and time steps for plotting purposes
 nmax = round(tf / dt);
@@ -39,37 +38,38 @@ Eu = zeros(1, 101);
 
 [F, f, fp] = compute_F(u);
 
-G = compute_G(u, epsilon, eta, F, f, k, dx, dy, nx, ny);
+G = compute_G(u, epsilon, eta, F, f, kxx, kyy, dx, dy, nx, ny, x, y);
 
 w_old = compute_w(G, B);
 
-Eu(1) = compute_E(u, w_old, epsilon, eta, B, dy, dy, k);
+Eu(1) = compute_E(u, w_old, epsilon, eta, B, dy, dy, k, kxx, kyy, nx, ny);
 
-H = compute_H(u, w_old, epsilon, eta, f, fp, x, y, xx, yy, nx, ny, k);
+H = compute_H(u, w_old, epsilon, eta, f, fp, x, y, xx, yy, nx, ny, kxx, kyy, k);
 H_old = H;
 
 r = w_old - 0.5 * sum(sum(H.*u))*dx*dy;
 
-H2 = fftn(H);
 
-v = fftn(u);
+H2 = fft2(H);
+
+v = fft2(u);
 
 r_hat = u/dt + (epsilon^2*(2 + eta) + S) * ...
-    real(ifftn(reshape(k.^4 .* v(:), nx, ny))) + ...
-    r * real(ifftn( reshape(-k.^2 .* H(:), nx, ny)));
+    real(ifft2(k.^4 .* v)) + ...
+    r * real(ifft2(-k.^2 .* H));
 
 
 % define linear operator for BDF1
-P = spdiags(1/dt + epsilon^4*k.^6 + S*k.^4, 0, nx*ny, nx*ny);
+P = spdiags(1/dt + epsilon^4*k(:).^6 + S*k(:).^4, 0, nx*ny, nx*ny);
 
-r_hat = fftn(r_hat);
+r_hat = fft2(r_hat);
 psi_r = P \ r_hat(:);
-psi_r = real(ifftn(reshape(psi_r, nx, ny)));
+psi_r = real(ifft2(reshape(psi_r, nx, ny)));
 
-psi_H = P \ (-k.^2 .* H2(:));         
-psi_H = real(ifftn(reshape(psi_H, nx, ny)));
+psi_H = P \ (-k(:).^2 .* H2(:));         
+psi_H = real(ifft2(reshape(psi_H, nx, ny)));
 
-innprod_Hu = compute_ip(H, psi_r, psi_H, dx, dy);
+innprod_Hu = compute_ip(H, psi_r, psi_H, dx, dy, x, y);
 
 w = innprod_Hu + r;
 
@@ -78,7 +78,7 @@ u_old = u;
 u = 0.5*innprod_Hu * psi_H + psi_r;
 
 % define P for BDF2
-P = spdiags(3/(2*dt) + epsilon^4*k.^6 + S*k.^4, 0, nx*ny, nx*ny);
+P = spdiags(3/(2*dt) + epsilon^4*k(:).^6 + S*k(:).^4, 0, nx*ny, nx*ny);
 
 if nplt == 1
     
@@ -88,7 +88,7 @@ if nplt == 1
 
     w_vals(2) = w;
     
-    Eu(2) = compute_E(u, w, epsilon, eta, B, dx, dy, k);
+    Eu(2) = compute_E(u, w, epsilon, eta, B, dx, dy, k, kxx, kyy, nx, ny);
     
     mass(2) = mean(u(:));
     
@@ -108,12 +108,12 @@ for j = 2 : nmax
 
     [F, f, fp] = compute_F(u_snew);
 
-    G = compute_G(u_snew, epsilon, eta, F, f, k, dx, dy, nx, ny);
+    G = compute_G(u_snew, epsilon, eta, F, f, kxx, kyy, dx, dy, nx, ny, x, y);
 
     w_snew = compute_w(G, B);
 
     H_snew = compute_H(u_snew, w_snew, epsilon, eta, f, fp, x, y,...
-        xx, yy, nx, ny, k);
+        xx, yy, nx, ny, kxx, kyy, k);
 
     % H_1 = compute_H(u, w, epsilon, eta, f, fp, x, y, xx, yy, nx, ny, k);
 
@@ -121,24 +121,25 @@ for j = 2 : nmax
 
     % H_old = H_1;
 
-    H = fftn(H_snew);
-    u_snew = fftn(u_snew);
+    H = fft2(H_snew);
+    u_snew = fft2(u_snew);
 
     r = (4*w - w_old)/3 - 0.5 * sum(sum(H_snew .* (4*u - u_old)/3)) ...
         * dx*dy;
 
-    r_hat = (4*u - u_old)/(2*dt) + (epsilon^2*(2 + eta) + S) * ...
-        real(ifftn(reshape(k.^4 .* u_snew(:), nx, ny))) + ...
-        r * real(ifftn( reshape(-k.^2 .* H(:), nx, ny)));
 
-    r_hat = fftn(r_hat);    
+    r_hat = (4*u - u_old)/(2*dt) + (epsilon^2*(2 + eta) + S) * ...
+        real(ifft2(k.^4 .* u_snew)) + ...
+        r * real(ifft2(-k.^2 .* H));
+
+    r_hat = fft2(r_hat);    
     
     psi_r = P \ r_hat(:);           psi_r = reshape(psi_r, nx, ny);
-    psi_r = real(ifftn(psi_r));
-    psi_H = P \ (-k.^2.*H(:));     psi_H = reshape(psi_H, nx, ny);
-    psi_H = real(ifftn(psi_H));
+    psi_r = real(ifft2(psi_r));
+    psi_H = P \ (-k(:).^2.*H(:));     psi_H = reshape(psi_H, nx, ny);
+    psi_H = real(ifft2(psi_H));
 
-    innprod_Hu = compute_ip(H_snew, psi_r, psi_H, dx, dy);
+    innprod_Hu = compute_ip(H_snew, psi_r, psi_H, dx, dy, x, y);
 
     w_new = 0.5*innprod_Hu + r; 
 
@@ -149,7 +150,7 @@ for j = 2 : nmax
 
     if (mod(j, nplt) == 0)                  
 
-        Eu(j/nplt + 1) = compute_E(u, w, epsilon, eta, B, dx, dy, k);
+        Eu(j/nplt + 1) = compute_E(u, w, epsilon, eta, B, dx, dy, k, kxx, kyy, nx, ny);
         
         mass(j/nplt + 1) = mean(u(:));
         
@@ -182,14 +183,21 @@ fp = 3 * u.^2 - 1;
 
 end
 
-function G = compute_G(u, epsilon, eta, F, f, k, dx, dy, nx, ny)
+function G = compute_G(u, epsilon, eta, F, f, kxx, kyy, dx, dy, nx, ny, x, y)
 
-k = reshape(k, nx, ny);
+v = fft2(u);
 
-v = fftn(u);
+ux = ifft2(-1i*kxx.*v);         
+uy = ifft2(-1i*kyy.*v);
 
-G = sum(sum(3*epsilon^2 * u.^2 .* real(ifftn(-1i*k.*v)).^2 + ...
+% [ux, uy] = gradient(u, x, y);
+
+G = sum(sum(3*epsilon^2 * u.^2 .* (ux.^2 + uy.^2) + ...
     0.5 * f.^2 - eta * F)) *  dx*dy;
+% 
+% G = sum(sum(3*epsilon^2 * u.^2 .* real(ifftn(-1i*k.*v)).^2 + ...
+%     0.5 * f.^2 - eta * F)) *  dx*dy;
+
 
 end
 
@@ -199,39 +207,58 @@ w = sqrt(G + B);
 
 end
 
-function H = compute_H(u, w, epsilon, eta, f, fp, x, y, xx, yy, nx, ny, k)
+function H = compute_H(u, w, epsilon, eta, f, fp, x, y, xx, yy, nx, ny, kxx, kyy, k)
 
-k = reshape(k, nx, ny);
-v = fftn(u);
+v = fft2(u);
+v2 = fft2(u.^2);
 
-[ux, uy] = gradient(u, x, y);
+ux = real(ifft2(-1i*kxx.*v));         
+uy = real(ifft2(-1i*kyy.*v));
+
+ux2 = real(ifft2(-1i*kxx.*v2));         
+uy2 = real(ifft2(-1i*kyy.*v2));
+
+% [ux, uy] = gradient(u, x, y);
 % [ux2, uy2] = gradient(u.^2, x, y);
 
-div_u = divergence(xx, yy, u.^2.*ux, u.^2.*uy);
+% div_u = divergence(xx, yy, u.^2.*ux, u.^2.*uy);
 % u_div_u = u.^2 .* (divergence(xx, yy, ux, uy));
 
-H = (6*epsilon^2 * (u.* real(ifftn(-1i*k.*v)).^2 - div_u) + f .* fp - ...
-    eta * f) / w;
+% H = (6*epsilon^2 * (u.* real(ifftn(-1i*k.*v)).^2 - div_u) + f .* fp - ...
+%     eta * f) / w;
+
+u_grad1 = ux.^2 + uy.^2;
+
+H = (6*epsilon^2 * (u.* u_grad1 -  ux.*ux2 - uy.*uy2 - ...
+    u.^2 .* real(ifft2(-k.^2 .* v))) + f .* fp - eta * f) / w;
 
 % H = (6*epsilon^2 * (u.* real(ifftn(-1i*k.*v)).^2 - (u_div_u + ux.*ux2 + ...
 %     uy .* uy2)) + f .* fp - eta * f) / w;
 
+% H = (6*epsilon^2 * (u.^2 .* real(ifftn(-k.^2.*v)) -...
+%     2*u.* real(ifftn(-1i*k.*v)).^2) + f .* fp - eta * f) / w;
+
 end
 
 
-function innprod_Hu = compute_ip(H_snew, psi_r, psi_H, dx, dy)
+function innprod_Hu = compute_ip(H_snew, psi_r, psi_H, dx, dy, x, y)
 
 innprod_Hu = sum(sum(H_snew .* psi_r))*dx*dy / ...
     (1 - 0.5*sum(sum(H_snew .* psi_H)) * dx*dy);
 
+
 end
 
-function E = compute_E(u, w, epsilon, eta, B, dx, dy, k)
+function E = compute_E(u, w, epsilon, eta, B, dx, dy, k, kxx, kyy, nx, ny)
 
-u = fftn(u);
+u = fft2(u);
+ux = real(ifft2(-1i*kxx.*u));         
+uy = real(ifft2(-1i*kyy.*u));
 
-E = sum(sum(0.5*epsilon^4 * (real(ifftn(-k.^2 .* u(:)))).^2 - ...
-    (eta/2 + 1)*epsilon^2 * real(ifftn(-1i*k.*u(:))).^2))*...
+du = ux.^2 + uy.^2;
+
+E = sum(sum(0.5*epsilon^4 * (real(ifft2(-k.^2 .* u))).^2 - ...
+    (eta/2 + 1)*epsilon^2 * du))*...
     dx*dy + w^2 - B;
 
 end
