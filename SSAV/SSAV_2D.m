27a56aa725cdef50d144834fbbde368d7742ae19
Suki Shergill <sks78@sfu.ca>
2024-12-16 14:10:15 -0800

    Update SSAV_2D.m

--- SSAV/SSAV_2D.m
+++ SSAV/SSAV_2D.m
@@ -33,14 +33,17 @@ function [u, Eu, Em, mass, m_est_vals, t_vals, dt_vals]...
 %           2. Phase-field-crystals
 %           3. Allen-Cahn
 
-err_tol = 1E-5;        % error tolerance
+err_tol = 1E-3;        % error tolerance
 
 dt = Time.dt_min;
+
+k2 = Grid.kxx.^2 + Grid.kyy.^2;
+k4 = k2.^2;         k6 = k2.^3.;
 
 
 if model == 2
 
-    D = (-Grid.k.^2 + 1);    % PFC
+    D = (-k2 + 1);    % PFC
     
 else
     
@@ -53,11 +56,9 @@ if model == 3
     G = -1;             % AC
     
 else
-    G = -Grid.k.^2;          % PFC, CH and OK
+    G = -k2;          % PFC, CH and OK
     
-end
-    
-v = fft2(u);
+end    
 
 % define number of time steps and time steps for plotting purposes
 nplt = floor( (Time.tf / 100) / Time.dt_min);
@@ -68,11 +69,11 @@ nmax = round(Time.tf / Time.dt_min);
 %%%%%%%% Compute u^1 using backward Euler %%%%%%%% 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-F = compute_F(u, Para.beta);         int_F = sum(F(:)) * Grid.dx*Grid.dy;
-f = compute_f(u, Para.beta);
+[F, f] = compute_F(u);         int_F = sum(F(:)) * Grid.dx*Grid.dy;
+% f = compute_f(u, Para.beta);
 
 % define the following as w_old b/c we will need this in the for loop
-w_old = compute_w(int_F, Para.B);   
+w_old = compute_w(int_F);   
 
 t = 0;
 
@@ -82,20 +83,26 @@ mass(1) = (sum(u(:))*Grid.dx*Grid.dy)/(Grid.Lx*Grid.Ly);
 
 m_est_vals = zeros(1, 100);
 
-Em = Grid.Lx*Grid.Ly * compute_F(Para.m, Para.beta);
+Em = Grid.Lx*Grid.Ly * compute_F(Para.m);
 
 Eu = zeros(1, nmax);
+Eu_SSAV = zeros(1, nmax);
+Eu_SSAV_test = zeros(1, nmax);
+u_t = zeros(1, nmax-1);
+E_t = zeros(1, nmax-1);
+E_tt = zeros(1, nmax-1);
 t_vals = zeros(1, nmax);
 
-Eu(1) = compute_En(u, w_old, Para, Grid.dx, Grid.dy, Grid.inv_k,...
-    Grid.k, Em, D, model, Grid.kxx, Grid.kyy, Grid.Nx, Grid.Ny);
+Eu(1) = compute_En(u, w_old);
+Eu_SSAV_test(1) = (Eu(1))/2 + (compute_En(2*u - u, 2*w_old - w_old)) / 2 + ...
+    sum(Para.S/2 * (u - u).^2, 'all')*Grid.dx*Grid.dy;
 
 H = compute_H(f, w_old);
 
-r = -0.5 * sum(sum(H.*u)) * Grid.dx*Grid.dy + w_old;
+r = -0.5 * sum(H.*u, 'all') * Grid.dx*Grid.dy + w_old;
 H2 = fft2(H);
 
-r_tilde = u/dt - Para.S*real(ifft2(G .* v)) - r*real(ifft2(G .* H2));
+r_tilde = u/dt - Para.S*real(ifft2(G .* ufft)) - r*real(ifft2(G .* H2));
 
 r_hat = (Para.alpha*Para.epsilon^2*dt)/(Grid.Lx*Grid.Ly) * ...
     sum(r_tilde(:))*Grid.dx*Grid.dy + r_tilde;
@@ -124,7 +131,7 @@ psi_r = real(ifft2(reshape(psi_r, Grid.Nx, Grid.Ny)));
 psi_H = P \ (G(:) .* H2(:)); 
 psi_H = real(ifft2(reshape(psi_H, Grid.Nx, Grid.Ny)));
 
-innprod_Hu = compute_ip(H, psi_r, psi_H, Grid.dx, Grid.dy);
+innprod_Hu = compute_ip(H, psi_r, psi_H);
 
 w = 0.5*innprod_Hu + r;
 
@@ -136,9 +143,14 @@ dt_vals = dt;
 
 t = t + dt;
 
+Eu(2) = compute_En(u, w);
+Eu_SSAV_test(2) = (Eu(2))/2 + (compute_En(2*u - u, 2*w - w)) / 2 + ...
+    sum(sum(Para.S/2 * (u(:) - u(:)).^2))*Grid.dx*Grid.dy;
+Eu_SSAV(1) = (Eu(2))/2 + (compute_En(2*u - u_old, 2*w - w_old)) / 2 + ...
+    sum(sum(Para.S/2 * (u(:) - u_old(:)).^2))*Grid.dx*Grid.dy;
 
-Eu(2) = compute_En(u, w, Para, Grid.dx, Grid.dy, Grid.inv_k, Grid.k, Em, D, model, Grid.kxx, Grid.kyy, Grid.Nx, Grid.Ny);
 E_old = Eu(1);       E = Eu(2);       t_vals(2) = t;
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%% Main time loop %%%%%%%%%%%%%%%%% 
@@ -156,53 +168,34 @@ while t < Time.tf
     t = t + dt_new;
 
     [u_new, w_new, m_est, gamma] = compute_unew(u, u_old, w, w_old, ...
-        Grid, dt, dt_new, Para, G, D,...
-        model, P1);
+        dt, dt_new);
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%% Adaptive time stepping %%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    E_new = compute_En(u, w, Para, Grid.dx, Grid.dy, Grid.inv_k, Grid.k,...
-        Em, D, model, Grid.kxx, Grid.kyy, Grid.Nx, Grid.Ny);
+    E_new = compute_En(u_new, w_new);
 
-    ut = sum(sum(((u_new - u_old) / (dt_new + dt)).^2))*Grid.dx * Grid.dy;
+    E_mod = E_new/2 + (compute_En(2*u_new - u, 2*w_new - w)) / 2 + ...
+        sum(sum(Para.S/2 * (u(:) - u_old(:)).^2))*Grid.dx*Grid.dy;
 
-    Et = (E_new - E_old) / (dt_new + dt);
-
-    Ett = ((dt/dt_new)*E_new - (dt/dt_new + 1)*E + E_old) / ...
-        (0.5 * (dt^2 + dt*dt_new));
-
-    rel_err = 0.5*dt_new*abs(Et) + dt_new^2 * (Para.S/2 * ut + ...
-        abs(Ett));
-        %(Ett < 0)*abs(Ett));
-    rel_err = rel_err / abs(E_new);
+    rel_err = abs(E_new - E_mod);
 
     adap = 0.5*dt*(err_tol / rel_err);
-
 
     dt_new = max(Time.dt_min, min(adap, Time.dt_max));
 
     while (rel_err > err_tol) && (dt_new ~= Time.dt_min)
 
         [u_new, w_new, m_est, gamma] = compute_unew(u, u_old, w, w_old, ...
-        Grid, dt, dt_new, Para, G, D,...
-        model, P1);
+            dt, dt_new);
 
-        E_new = compute_En(u, w, Para, Grid.dx, Grid.dy, Grid.inv_k, Grid.k, Em, D, model, Grid.kxx, Grid.kyy, Grid.Nx, Grid.Ny);
+        E_new = compute_En(u_new, w_new);
         
-        % E_new = Eu(j + 1);
+   
+        E_mod = E_new/2 + (compute_En(2*u_new - u, 2*w_new - w)) / 2 + ...
+            sum(sum(Para.S/2 * (u(:) - u_old(:)).^2))*Grid.dx*Grid.dy;
     
-        ut = sum(sum(((u_new - u_old) / (dt_new + dt)).^2))*Grid.dx * Grid.dy;
-    
-        Et = (E_new - E_old) / (dt_new + dt);
-    
-        Ett = ((dt/dt_new)*E_new - (dt/dt_new + 1)*E + E_old) / ...
-            (0.5 * (dt^2 + dt*dt_new));
-    
-        rel_err = 0.5*dt_new*abs(Et) + dt_new^2 * (Para.S/2 * ut + ...
-            abs(Ett));
-            %(Ett < 0)*abs(Ett));
-        rel_err = rel_err / abs(E_new);
+        rel_err = abs(E_new - E_mod);
 
         adap = 0.5*dt*(err_tol / rel_err);
 
@@ -214,6 +207,10 @@ while t < Time.tf
  
     Eu(j + 1) = E_new;
 
+    Eu_SSAV_test(j+1) = (Eu(j+1))/2 + (compute_En(2*u - u, 2*w - w)) / 2 + ...
+        sum(sum(Para.S/2 * (u(:) - u(:)).^2))*Grid.dx*Grid.dy;
+    Eu_SSAV(j) = E_mod;
+
     w_old = w;                      w = w_new;
     u_old = u;                      u = u_new;
     E_old = E;                      E = E_new;
@@ -224,10 +221,9 @@ end
 
 nt = sum(t_vals > 0) + 1;
 
-Em = Em / (Grid.Lx*Grid.Ly);
-Eu = Eu(1:nt) ./ (Grid.Lx*Grid.Ly);
+% Em = Em; % / (Grid.Lx*Grid.Ly);
+Eu = Eu(1:nt); %./ (Grid.Lx*Grid.Ly);
 t_vals = t_vals(1:nt);
-
 
 function u_snew = compute_u_snew(u, u_old)
 
@@ -235,21 +231,23 @@ u_snew = 2*u - u_old;
 
 end
 
-function f = compute_f(u, beta)
+function [F, f] = compute_F(u)
 
-f = u.^3 - beta*u;
+f = u.^3 - Para.beta*u;
 
-end
-
-function F = compute_F(u, beta)
-
-F = 0.25 * (u.^2 - beta).^2;
+F = 0.25 * (u.^2 - Para.beta).^2;
 
 end
 
-function w = compute_w(int_F, B)
+% function F = compute_F(u, beta)
+% 
+% F = 0.25 * (u.^2 - beta).^2;
+% 
+% end
 
-w = sqrt(int_F + B);
+function w = compute_w(int_F)
+
+w = sqrt(int_F + Para.B);
 
 end
 
@@ -260,7 +258,7 @@ H = f / w;
 end
 
 function [r, r_hat, m_est] = compute_r(u_snew, u, u_old, w, w_old, ... 
-    H_snew, S, Grid, dt, alpha, epsilon, a, b, c, G)
+    H_snew, dt, a, b, c)
 
 r = -(b*w + c*w_old)/a + 0.5 * sum(sum((H_snew .* (b*u + c*u_old)/a)))...
     * Grid.dx*Grid.dy;
@@ -268,10 +266,10 @@ r = -(b*w + c*w_old)/a + 0.5 * sum(sum((H_snew .* (b*u + c*u_old)/a)))...
 rH = fft2(r .* H_snew);
 u_snew = fft2(u_snew);
 
-r_tilde = -(b*u + c*u_old) - S*real(ifft2(G .* u_snew)) + ...
+r_tilde = -(b*u + c*u_old) - Para.S*real(ifft2(G .* u_snew)) + ...
     real(ifft2(G .* rH));
 
-m_est = (alpha*epsilon^2)/(a*Grid.Lx*Grid.Ly) * ...
+m_est = (Para.alpha*Para.epsilon^2)/(a*Grid.Lx*Grid.Ly) * ...
     sum(r_tilde(:)) * Grid.dx*Grid.dy;
 
 r_hat = r_tilde + m_est;
@@ -279,25 +277,25 @@ r_hat = r_tilde + m_est;
 end
 
 
-function innprod_Hu = compute_ip(H_snew, psi_r, psi_H, dx, dy)
+function innprod_Hu = compute_ip(H_snew, psi_r, psi_H)
 % This fuction computes the inner product of H^{*,n+1} and u^{n+1}
 
-innprod_Hu = sum(sum(H_snew .* psi_r)) * dx*dy...
-    / (1 - 0.5*sum(sum(H_snew .* psi_H)) * dx*dy);
+innprod_Hu = sum(sum(H_snew .* psi_r)) * Grid.dx*Grid.dy...
+    / (1 - 0.5*sum(sum(H_snew .* psi_H)) * Grid.dx*Grid.dy);
 
 end
 
-function E_n = compute_En(u, w, Para, dx, dy, inv_k, k, Em, D, model, kxx, kyy, Nx, Ny)
+function E_n = compute_En(u, w)
 
 e = Para.epsilon^2/2;
 
 um = fft2(u - Para.m);
 
-v = -inv_k .* um;
-v(k == 0) = 0;
+v = -Grid.inv_k .* um;
+v(Grid.k == 0) = 0;
 
-vx = real(ifft2(-1i*kxx.*v));         
-vy = real(ifft2(-1i*kyy.*v));
+vx = real(ifft2(-1i*Grid.kxx.*v));         
+vy = real(ifft2(-1i*Grid.kyy.*v));
 
 dv = vx.^2 + vy.^2;
 
@@ -308,13 +306,13 @@ if model == 2
     du = D.*u;
 else
 
-    ux = real(ifft2(-1i*kxx.*u));         
-    uy = real(ifft2(-1i*kyy.*u));
+    ux = real(ifft2(-1i*Grid.kxx.*u));         
+    uy = real(ifft2(-1i*Grid.kyy.*u));
 
     du = ux.^2 + uy.^2;
 end
 
-E_n = sum(sum(e *du + Para.alpha*e * dv))*dx*dy +  w^2 - Para.B;
+E_n = sum(sum(e *du + Para.alpha*e * dv))*Grid.dx*Grid.dy +  w^2 - Para.B;
 
 end
 
@@ -331,63 +329,59 @@ c = gamma^2 / (1 + gamma);                  c = c / dt_new;
 end
 
 
-function u_p = AM3_up(u_new, u, u_old, epsilon, alpha, gamma, k, ...
-    dt_new, m, Nx, Ny, beta)
+function u_AM = AM3_up(u_new, u, u_old, gamma, dt_new)
 
-unew_fft = fft2(u_new);     fnew_fft = fft2(compute_f(u_new, beta));   
+unew_fft = fft2(u_new);     fnew_fft = fft2(compute_F(u_new));   
 
-u_fft = fft2(u);            f_fft = fft2(compute_f(u, beta));                        
+u_fft = fft2(u);            f_fft = fft2(compute_F(u));                        
 
-uold_fft = fft2(u_old);     fold_fft = fft2(compute_f(u_old, beta));
+uold_fft = fft2(u_old);     fold_fft = fft2(compute_F(u_old));
 
-F_tilde_unew = compute_F_tilde(unew_fft, u_new, fnew_fft, k, epsilon, ...
-    alpha, m, Nx, Ny);
+F_tilde_unew = compute_F_tilde(unew_fft, u_new, fnew_fft);
 
-F_tilde_u = compute_F_tilde(u_fft, u, f_fft, k, epsilon, ...
-    alpha, m, Nx, Ny);
+F_tilde_u = compute_F_tilde(u_fft, u, f_fft);
 
-F_tilde_uold = compute_F_tilde(uold_fft, u_old, fold_fft, k, epsilon, ...
-    alpha, m, Nx, Ny);
+F_tilde_uold = compute_F_tilde(uold_fft, u_old, fold_fft);
 
-u_p = u + (dt_new / 6) * (((3 + 2*gamma)/(1 + gamma))*F_tilde_unew + ...
+u_AM = u + (dt_new / 6) * (((3 + 2*gamma)/(1 + gamma))*F_tilde_unew + ...
     (3 + gamma)*F_tilde_u - (gamma^2 / (1 + gamma))*F_tilde_uold);
 
 end
 
-function F_tilde = compute_F_tilde(u_fft, u, f_fft, k, epsilon, alpha, m, Nx, Ny)
+function F_tilde = compute_F_tilde(u_fft, u, f_fft)
 
-F_tilde = -epsilon^2 * real(ifft2(k.^4 .* u_fft)) + ...
-    real(ifft2(-k.^2 .* f_fft)) + ...
-    alpha * epsilon * (u - m);
+F_tilde = -Para.epsilon^2 * real(ifft2(k4 .* u_fft)) + ...
+    real(ifft2(-k2 .* f_fft)) + ...
+    Para.alpha * Para.epsilon * (u - Para.m);
 
 end
 
-function A_dp = compute_A_dp(rho_s, err_tol, err, dt)
+function A_dp = compute_A_dp(rel_err, dt)
 
-A_dp = rho_s * dt * (err_tol / err) ^ (1/3);
+A_dp = rho_s * dt * (err_tol / rel_err) ^ (1/3);
 
 end
 
 function [u_new, w_new, m_est, gamma] = compute_unew(u, u_old, w, w_old,...
-    Grid, dt, dt_new, Para, G, D, model, P1)
+    dt, dt_new)
 
 u_snew = compute_u_snew(u, u_old);
     
-F = compute_F(u_snew, Para.beta);
-f = compute_f(u_snew, Para.beta);
+[F, f] = compute_F(u_snew);
+% f = compute_f(u_snew, Para.beta);
 
 int_F = sum(F(:)) * Grid.dx*Grid.dy;
 
 %w(u^{*,n+1})
-w_snew = compute_w(int_F, Para.B);
+w_snew = compute_w(int_F);
 
 % H^{*,n+1}
 H_snew = compute_H(f, w_snew);
 
 [a, b, c, gamma] =  compute_dt_coeffs(dt_new, dt);
 
-[r, r_hat, m_est] = compute_r(u_snew, u, u_old, w, w_old, H_snew, Para.S, ...
-    Grid, dt_new, Para.alpha, Para.epsilon, a, b, c, G);
+[r, r_hat, m_est] = compute_r(u_snew, u, u_old, w, w_old, H_snew, ...
+    dt_new, a, b, c);
 
 % define P for BDF2    
 P = spdiags(a + P1, 0, Grid.Nx*Grid.Ny, Grid.Nx*Grid.Ny);
@@ -400,7 +394,7 @@ H2 = fft2(H_snew);
 psi_H = P \ (G(:).*H2(:));     psi_H = reshape(psi_H, Grid.Nx, Grid.Ny);
 psi_H = real(ifft2(psi_H));
 
-innprod_Hu = compute_ip(H_snew, psi_r, psi_H, Grid.dx, Grid.dy);
+innprod_Hu = compute_ip(H_snew, psi_r, psi_H);
 
 w_new = 0.5*innprod_Hu + r;      
 
