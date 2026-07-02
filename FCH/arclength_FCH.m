e5cb010c24fdb920414f297a1407495e9fbe9c8b
Suki Shergill <sks78@sfu.ca>
2026-07-02 14:08:39 -0700

    Update arclength_FCH.m

--- FCH/arclength_FCH.m
+++ FCH/arclength_FCH.m
@@ -11,10 +11,11 @@ N = 2^7;
 Grid = SSAV_FCH_helpers.generate_Grid(L, N, dim);
 
 % initialize values
-m = 0;         % initial m
+m = 0.001;         % initial m
 
 % compute u, F(u), F'(u), F''(u) to compute initial E
-u = compute_u(Grid, m, eps);
+% u = compute_u(Grid, m, eps);
+u = Results.uu{end};
 uhat = fft(u);
 ck = uhat / N;
 
@@ -22,11 +23,15 @@ E = compute_E(u, uhat, eps, eta, Grid);       % initial E(u)
 
 data = [m; E];
 
-ds = 1/1024;
+ds = 1/1000;
 
 % initialize tangent vector
 t = zeros(N+1, 1);      t(1) = 1;       t(end) = 1;
-t = t / norm(t, 2);     % initial tangent vector;
+
+% t = initialize_t(m, 1/1000, ds, eps, eta, Grid);
+% t = rand(N+1, 1);
+
+t = t / norm(t, 2);         % initial tangent vector
 
 while m < 1
 
@@ -38,7 +43,7 @@ u_curr = ifft(ck_curr * N, 'symmetric');
 % compute corrector
 for j = 1:10
 
-    grad = compute_RHS(gradientinit(ck), u_curr, eps, eta, Grid);
+    grad = compute_RHS(gradientinit(ck_curr), u_curr, eps, eta, Grid);
     F_curr = grad.x;
     J = grad.dx;
 
@@ -93,10 +98,10 @@ xlim([0, 1])
 
 function u = compute_u(Grid, m, eps)
 
-% u = tanh((Grid.x + pi*(m + 1)/2) / eps) - ...
-%     tanh((Grid.x - pi*(m + 1)/2) / eps) - 1;
+u = tanh((Grid.x + pi*(m + 1)/2) / eps) - ...
+    tanh((Grid.x - pi*(m + 1)/2) / eps) - 1;
 
-u = m*ones(size(Grid.x));         % test case
+% u = m*ones(size(Grid.x));         % test case
 
 end
 
@@ -182,11 +187,28 @@ end
 
 function f = compute_RHS(ck, u, eps, eta, Grid)
 
-f = -eps^4 * Grid.k6 .* ck - ...
-    eps^2 * Grid.k4 .* ck.*(fft(u.^3) - 1) ...
-    - eps^2 * Grid.k4 .* ck.^2 .* (fft(3*u.^2) - 1) + ...
-    ck .* (fft(3*u.^5 - 4*u.^3) - 1) + ...
-    eta * eps^2 * Grid.k4 .* ck - ...
-    eta * Grid.k2 .* ck .* (fft(u.^3) - 1);
+f = eps^4 * Grid.k4 .* ck ...
+    + eps^2 * Grid.k2 .* ck .* (fft(u.^3) - 1) ...
+    + eps^2 * Grid.k2 .* ck.^1 .* (fft(3*u.^2 - 1)) ...
+    - ck .* (fft(3*u.^5 - 4*u.^3) + 1) ...
+    - eta * eps^2 * Grid.k2 .* ck ...
+    - eta * ck .* (fft(u.^3) - 1);
 
+end
+
+function t = initialize_t(m, dm, ds, eps, eta, Grid)
+
+up = compute_u(Grid, m + dm, eps);
+ckp = fft(up) / Grid.N;
+Fp = compute_RHS(ckp, up, eps, eta, Grid);
+
+um = compute_u(Grid, m - dm, eps);
+ckm = fft(up) / Grid.N;
+Fm = compute_RHS(ckm, um, eps, eta, Grid);
+
+dFdm = 2*(Fp - Fm) / dm;
+
+% dmds = dm / ds;
+
+t = [dFdm; dm];
 end
