1f6ef7b323be60c90b28d63379e7d1872eada36f
Suki Shergill <sks78@sfu.ca>
2026-07-11 02:14:05 +0000

    Edits to python SSAV code

--- Python/ssav_helpers.py
+++ Python/ssav_helpers.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 import numpy as np
-from numpy.fft import fft2, ifft2, fftfreq
+from numpy.fft import fftn, ifftn, fftfreq
 
 @dataclass
 class Grid:
@@ -94,12 +94,12 @@ def generate_Grid(L, N, dim):
 
 def compute_E(ufft, w, Grid, Para):
 
-    ux = np.real(ifft2(-1j * Grid.kxx * ufft))
-    uy = np.real(ifft2(-1j * Grid.kyy * ufft))
+    ux = np.real(ifftn(-1j * Grid.kxx * ufft))
+    uy = np.real(ifftn(-1j * Grid.kyy * ufft))
 
     du = ux**2 + uy**2
 
-    return np.sum(0.5 * Para.eps**2 * du) + w**2 - Para.B
+    return np.sum(0.5 * Para.eps**2 * du)*np.prod(Grid.d) + w**2 - Para.B
 
 def compute_us(u_curr, u_prev):
     return 2 * u_curr - u_prev
@@ -128,9 +128,9 @@ def compute_dt_coeffs(dt, dt_new):
 
 def compute_r(us, us_fft, u_curr, u_prev, w_curr, w_prev, Hs, H2, a, b, c, Grid, Para, G, t, dt):
 
-    r = -(b*w_curr + c*w_prev)-(b*w_curr + c*w_prev)/a + 0.5 * np.sum(Hs * (b*u_curr + c*u_prev)/a) * np.prod(Grid.d);
+    r = -(b*w_curr + c*w_prev)/a + 0.5 * np.sum(Hs * (b*u_curr + c*u_prev)/a) * np.prod(Grid.d);
 
-    r_tilde = -(b*u_curr + c*u_prev) - Para.S*np.real(ifft2(G*us_fft)) + r*np.real(ifft2(G*H2))
+    r_tilde = -(b*u_curr + c*u_prev) - Para.S*np.real(ifftn(G*us_fft)) + r*np.real(ifftn(G*H2))
 
     # m_est
 
@@ -159,18 +159,18 @@ def compute_unew(u_curr, ucurr_fft, u_prev, uprev_fft, w_curr, w_prev, dt, Para,
 
     a,b,c = compute_dt_coeffs(dt, dt)
 
-    H2 = fft2(Hs)
+    H2 = fftn(Hs)
 
     r, r_hat = compute_r(us, 2*ucurr_fft - uprev_fft, u_curr, u_prev, w_curr, w_prev, Hs, H2, a, b, c, Grid, Para, G, t + dt, dt)
 
     P = a + P1
 
-    r_hat = fft2(r_hat)
+    r_hat = fftn(r_hat)
     psi_r = r_hat / P
-    psi_r = np.real(ifft2(psi_r))
+    psi_r = np.real(ifftn(psi_r))
 
     psi_H = (G * H2) / P
-    psi_H = np.real(ifft2(psi_H))
+    psi_H = np.real(ifftn(psi_H))
 
     innprod_Hu = compute_ip(Hs, psi_r, psi_H, Grid)
 
